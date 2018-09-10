01147d1e05181c2e43afecbf4ce22ed14a36687d
hamilton <gabrielzinho.gabriel2@gmail.com>
2018-09-09 20:31:09 -0400

    Definindo o App como SPA, passando para os parametros router-link o caminho.

--- src/components/theme/AppHeader.vue
+++ src/components/theme/AppHeader.vue
@@ -1,10 +1,16 @@
 <template>
     <nav class="nav has-shadow">
         <div class="container">
-            <a href="/">
+            <router-link to="/" exact>
                 <img src="http://bit.ly/vue-img" alt="Vue SPA">
-            </a>
-        </div>
+            </router-link>
+            <div id="navbarExampleTransparentExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <router-link class="navbar-item" to="/" exact>Início</router-link>
+                    <router-link class="navbar-item" to="/login">Login</router-link>
+                </div>
+                </div>
+            </div>
     </nav>
 </template>
 
