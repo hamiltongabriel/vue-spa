5f99a8345635e85635ace94ab331227aef1aa781
hamilton <gabrielzinho.gabriel2@gmail.com>
2018-09-10 04:19:40 -0400

    Criando mensagem para a página não existente.

---
+++ src/components/theme/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Oops, página não existe.
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from '../components/theme/Category'
 import Login from '../components/theme/Login'
+import NotFound from '../components/theme/NotFound'
 
 Vue.use(VueRouter);
 
@@ -12,7 +13,8 @@ const router = new VueRouter({
     routes: [
         {path: '/login', component: Login},
         {path: '/category/:id', name: 'category', component: Category},
-        {path: '/', redirect: '/category/front-end'}
+        {path: '/', redirect: '/category/front-end'},
+        {path: '*', component: NotFound}
         ]
 });
 export default router
